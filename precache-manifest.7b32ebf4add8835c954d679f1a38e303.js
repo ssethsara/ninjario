32c9b2fc57695a03f17361a385b0a80e2c1c2374
ssethsara <sssethsara@gmail.com>
2023-10-30 02:19:16 +0530

    initial commit

---
+++ precache-manifest.7b32ebf4add8835c954d679f1a38e303.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0654385b184cc03d87faa2656afe84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a8e6bee589830e9a441",
+    "url": "/static/css/main.3d99ed05.chunk.css"
+  },
+  {
+    "revision": "e9a0eb81acb993c77215",
+    "url": "/static/js/2.964d8849.chunk.js"
+  },
+  {
+    "revision": "1ba3dc76a75254864014",
+    "url": "/static/js/3.21e50b1b.chunk.js"
+  },
+  {
+    "revision": "5a8e6bee589830e9a441",
+    "url": "/static/js/main.d3555d1f.chunk.js"
+  },
+  {
+    "revision": "ccf5a474a86f0ded1f4c",
+    "url": "/static/js/runtime~main.f805007c.js"
+  },
+  {
+    "revision": "6c02f15fdbc9dd7c482b52b06d8e0a6c",
+    "url": "/static/media/SuperMario256.6c02f15f.ttf"
+  }
+]);
